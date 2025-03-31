833eb9bfe2f75985ee41c0e3e6af20388183ca13
fady2497 <59845883+fady2497@users.noreply.github.com>
2025-03-31 17:46:39 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,21 +6,19 @@ app = Flask(__name__)
 
 @app.route('/<int:valeur>')
 def exercice(valeur):
-    # Premier motif
+   
     etoiles_1 = ''
     for j in range(valeur):
         for i in range(valeur - j):  
             etoiles_1 += '*'
         etoiles_1 += '<br>'
     
-    # Deuxième motif
     etoiles_2 = ''
     for j in range(valeur):
         for i in range(j + 1):  
             etoiles_2 += '*'
         etoiles_2 += '<br>'
     
-    # Retourner les deux motifs dans un format HTML
     return f"<pre>{etoiles_1}</pre><br><pre>{etoiles_2}</pre>"
 
 if __name__ == "__main__":
