c26f175f75edc57cbef895e50fb7f366a929193b
fady2497 <59845883+fady2497@users.noreply.github.com>
2025-03-31 10:49:40 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/<int:valeur>')
 def exercice(valeur):
-    etoiles = ''#comm2
+    etoiles = ''#comm
     for i in range(valeur):
         etoiles += '*'
     return etoiles
