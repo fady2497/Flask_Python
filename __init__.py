d8da0a785d0db985c0be0ae94ffa09e79cd4f5ab
fady2497 <59845883+fady2497@users.noreply.github.com>
2025-03-31 17:53:45 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,15 +1,15 @@
 from flask import Flask
 from flask import render_template
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route('/<int:valeur>')
 def exercice(valeur):
     etoiles = '<pre>'
     for j in range(1, valeur + 1):  
-        etoiles += ' ' * (valeur - j) + '*' * (2 * j - 1) + '\n'  
+        etoiles += ' ' * (valeur - j) + '*' * (1 * j - 1) + '\n'  
     etoiles += '</pre>'
     return etoiles
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     app.run(debug=True)
