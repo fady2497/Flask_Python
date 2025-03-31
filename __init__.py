fa4046949bbf3380d8c61535f43dd9128de05655
fady2497 <59845883+fady2497@users.noreply.github.com>
2025-03-31 17:04:17 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,17 +1,15 @@
 from flask import Flask
 from flask import render_template
 from flask import json
+app = Flask(name)
 
-app = Flask(__name__)
 
 @app.route('/<int:valeur>')
 def exercice(valeur):
     etoiles = ''
-    for j in range(valeur, 0, -1):  
-        for i in range(j):
-            etoiles += '*'
-        etoiles += '<br>'
+    for j in range(1, valeur + 1):
+        etoiles += '' j + '<br>'
     return etoiles
 
-if __name__ == "__main__":
+if name == "main":
     app.run(debug=True)
