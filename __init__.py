c360e9596923cb5796f14b4ad71c82a9fda55486
fady2497 <59845883+fady2497@users.noreply.github.com>
2025-03-31 17:27:31 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,9 +7,8 @@ app = Flask(__name__)
 @app.route('/<int:valeur>')
 def exercice(valeur):
     etoiles = '<pre>'
-    for j in range(1, valeur + 1):  
-        espaces = ' ' * (valeur - j)  
-        etoiles += espaces + '*' * (1 * j - 1) + '\n'
+    for j in range(1, valeur + 1): 
+        etoiles += ('*' * (1 * j - 1)).rjust(valeur + j - 1) + '\n'  
     etoiles += '</pre>'
     return etoiles
 
