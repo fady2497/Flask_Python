ec29f3c592ec3699eb7e8a3965d6c03a13b6a79f
fady2497 <59845883+fady2497@users.noreply.github.com>
2025-04-07 12:09:02 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,11 +2,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/<int:valeur>')
-def exercice2(valeur):
-    return ', '.join(map(str, 
-        (lambda n: [0, 1] + [sum([0, 1][max(0, i-2):i]) for i in range(2, n)])(valeur)
-    ))
+@app.route('/<int:n>')
+def calcul_somme(n):
+    somme = 0
+    for i in range(1, n + 1):
+        if i % 11 == 0:
+            continue
+        if i % 5 == 0 or i % 7 == 0:
+            somme += i
+        if somme > 5000:
+            break
+    return f"La somme finale est : {somme}"
 
 if __name__ == '__main__':
     app.run(debug=True)
