f1a432acfb9d0468edd737405c9db8b4156e0bc0
fady2497 <59845883+fady2497@users.noreply.github.com>
2025-04-07 12:18:48 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,18 +5,23 @@ app = Flask(__name__)
 @app.route('/<int:n>')
 def calcul_somme(n):
     somme = 0
+    dernier_nombre = 0 
+
     for i in range(1, n + 1):
+   
         if i % 11 == 0:
             continue  
-
-      
+        
+ 
         if i % 5 == 0 or i % 7 == 0:
             somme += i
+            dernier_nombre = i
 
-           
+          
             if somme > 5000:
-                return f"La somme a dépassé 5000 et est : {somme}"
-    return f"La somme finale est : {somme}"
+                return f"La somme a dépassé 5000 et est : {somme}. Le dernier nombre ajouté est : {dernier_nombre}"
+
+    return f"La somme finale est : {somme}. Le dernier nombre ajouté est : {dernier_nombre}"
 
 if __name__ == '__main__':
     app.run(debug=True)
