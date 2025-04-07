b4b94dd69e851f36764e1481fee826bf34f58de2
fady2497 <59845883+fady2497@users.noreply.github.com>
2025-04-07 12:13:58 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,11 +7,11 @@ def calcul_somme(n):
     somme = 0
     for i in range(1, n + 1):
         if i % 11 == 0:
-            continue  
+            continue 
         if i % 5 == 0 or i % 7 == 0:
             somme += i
             if somme > 5000:
-                break 
+                return f"La somme a dépassé 5000 et est : {somme}"
     return f"La somme finale est : {somme}"
 
 if __name__ == '__main__':
