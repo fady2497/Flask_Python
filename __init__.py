befc6b6eb38c4ec4d68d95421a6e274ebb612395
fady2497 <59845883+fady2497@users.noreply.github.com>
2025-04-07 09:35:57 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,10 +7,8 @@ def exercice(valeur):
     pyramide = ''
     for i in range(1, valeur + 1):
         ligne = ''
-        # Partie croissante
         for j in range(1, i + 1):
             ligne += str(j)
-        # Partie décroissante
         for j in range(i - 1, 0, -1):
             ligne += str(j)
         pyramide += ligne + '<br>'
