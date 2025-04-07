c11aee96f33ffe852db5b1990e17a11c56541a78
fady2497 <59845883+fady2497@users.noreply.github.com>
2025-04-07 14:39:04 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,10 +6,12 @@ app = Flask(__name__)
 def exercice(valeurs):
     liste_nombres = valeurs.split('/')
     liste_nombres = [int(n) for n in liste_nombres]
-    resultat = 0
-    for n in liste_nombres:
-        resultat = resultat + n
-    return str(resultat)
+
+    for i in range(len(liste_nombres) - 1):
+        if liste_nombres[i] > liste_nombres[i + 1]:
+            liste_nombres[i + 1] = liste_nombres[i]
+
+    return f"Le nombre maximum est : {liste_nombres[-1]}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
