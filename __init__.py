fcfcf557106518a1185c114b5efa89e5c37d34f0
fady2497 <59845883+fady2497@users.noreply.github.com>
2025-04-07 11:13:04 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,10 +4,9 @@ app = Flask(__name__)
 
 @app.route('/<int:valeur>')
 def exercice2(valeur):
-    suite = [0, 1]  
-    for _ in range(2, valeur):
-        suite += [suite[-1] + suite[-2]]  
-    return ', '.join(map(str, suite)) 
+    suite = [0, 1]
+    [suite.append(suite[-1] + suite[-2]) for _ in range(2, valeur)]
+    return ', '.join(map(str, suite))
 
 if __name__ == '__main__':
     app.run(debug=True)
