ef230dc625e9269ee6000d04100f3014c374fcf2
fady2497 <59845883+fady2497@users.noreply.github.com>
2025-04-07 14:44:07 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,11 +7,13 @@ def exercice(valeurs):
     liste_nombres = valeurs.split('/')
     liste_nombres = [int(n) for n in liste_nombres]
 
-    for i in range(len(liste_nombres) - 1):
-        if liste_nombres[i] > liste_nombres[i + 1]:
-            liste_nombres[i + 1] = liste_nombres[i]
+    plus_grand = None
 
-    return f"Le nombre maximum est : {liste_nombres[-1]}"
+    for nombre in liste_nombres:
+        if plus_grand is None or nombre > plus_grand:
+            plus_grand = nombre
+
+    return f"Le nombre maximum est : {plus_grand}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
