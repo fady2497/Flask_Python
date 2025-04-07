9d3a260685311c7f5ddea336defbeea79677da02
fady2497 <59845883+fady2497@users.noreply.github.com>
2025-04-07 10:53:49 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,18 +1,28 @@
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.route('/suite/<int:n>')
-def suite(n):
+@app.route('/<int:valeur>')
+def exercice(valeur):
+    etoiles = ''
+
+    for j in range(1, valeur + 1):
+        etoiles += '&nbsp;' * (valeur - j)
+        for i in range(1, j + 1):
+            etoiles += str(i)
+        for i in range(j - 1, 0, -1):
+            etoiles += str(i)
+        etoiles += '<br>'
+
     a, b = 0, 1
-    suite = []
+    suite = [a, b]
+    for _ in range(2, valeur):
+        suivant = suite[-1] + suite[-2]
+        suite.append(suivant)
 
-    for _ in range(n):
-        suite.append(str(a))
-        a, b = b, a + b
+    etoiles += '<br><strong>Exercice 2 : Suite de calculs</strong><br>'
+    etoiles += ', '.join(map(str, suite))
 
-    resultat = ', '.join(suite)
-    return f"<pre>{resultat}</pre>"
+    return etoiles
 
 if __name__ == "__main__":
     app.run(debug=True)
